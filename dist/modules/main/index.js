5e68ca0f76cd0d92e2b6f83b28451a487e94a085
celina-ijs <celina.dev@instajs.com>
2022-12-02 10:57:25 +0700

    Update to use @ijstech/cli

---
+++ dist/modules/main/index.js
@@ -0,0 +1,231 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+define("@carousel/main/data.json.ts", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    ///<amd-module name='@carousel/main/data.json.ts'/> 
+    const productList = [
+        {
+            img: 'https://images.unsplash.com/photo-1612404730960-5c71577fca11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFyaW98ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/JrZvYuBYzCU'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1586527155314-1d25428324ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fGdhbWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/aYPtEknQmXE'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1545927088-dab09318f32e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTEwfHxnYW1lfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/9QnFO9ezytU'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1594652634010-275456c808d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAxfHxnYW1lfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/i3WlrO7oAHA'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdhbWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/cPF2nlWcMY4'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1551103782-8ab07afd45c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdhbWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/_R95VMWyn7A'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1612404730960-5c71577fca11?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bWFyaW98ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/JrZvYuBYzCU'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1586527155314-1d25428324ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fGdhbWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/aYPtEknQmXE'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1545927088-dab09318f32e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTEwfHxnYW1lfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/9QnFO9ezytU'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1594652634010-275456c808d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTAxfHxnYW1lfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/i3WlrO7oAHA'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1472457897821-70d3819a0e24?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGdhbWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/cPF2nlWcMY4'
+        },
+        {
+            img: 'https://images.unsplash.com/photo-1551103782-8ab07afd45c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGdhbWV8ZW58MHwwfDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
+            url: 'https://unsplash.com/photos/_R95VMWyn7A'
+        },
+    ];
+    exports.default = productList;
+});
+define("@carousel/main/index.css.ts", ["require", "exports", "@ijstech/components"], function (require, exports, components_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = components_1.Styles.style({
+        $nest: {
+            'i-carousel-slider.--indicators .dots-pagination': {
+                display: 'flex',
+                marginBottom: '1rem'
+            },
+            'i-carousel-slider .dots-pagination': {
+                display: 'none'
+            },
+            '.--carousel-item > img': {
+                width: '100%'
+            },
+            '.--arrow-button': {
+                boxShadow: 'none',
+                $nest: {
+                    '& > span': {
+                        display: 'none'
+                    },
+                    '&:not(.disabled):hover': {
+                        background: 'transparent',
+                        boxShadow: 'none',
+                        $nest: {
+                            '> i-icon': {
+                                fill: 'rgba(117,124,131,.68) !important'
+                            }
+                        }
+                    }
+                }
+            },
+        }
+    });
+});
+define("@carousel/main/config.tsx", ["require", "exports", "@ijstech/components"], function (require, exports, components_2) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    let Config = class Config extends components_2.Module {
+        get data() {
+            const _data = {
+                autoplay: this.checkAutoplay.checked,
+                controls: this.checkControls.checked,
+                indicators: this.checkIndicators.checked,
+                swipe: this.checkSwipe.checked
+            };
+            const slidesToShow = Number(this.edtSlidesToShow.value);
+            if (Number.isInteger(slidesToShow))
+                _data.slidesToShow = slidesToShow;
+            return _data;
+        }
+        set data(config) {
+            this.edtSlidesToShow.value = config.slidesToShow || "";
+            this.checkAutoplay.checked = config.autoplay;
+            this.checkControls.checked = config.controls;
+            this.checkIndicators.checked = config.indicators;
+            this.checkSwipe.checked = config.swipe;
+        }
+        render() {
+            return (this.$render("i-vstack", { id: "pnlConfig", gap: '0.5rem', padding: { top: '1rem', bottom: '1rem', left: '1rem', right: '1rem' } },
+                this.$render("i-label", { caption: "Slides to show:" }),
+                this.$render("i-input", { id: "edtSlidesToShow", width: "100%", inputType: 'number' }),
+                this.$render("i-checkbox", { id: "checkAutoplay", caption: "Autoplay?" }),
+                this.$render("i-checkbox", { id: "checkControls", caption: "Controls?" }),
+                this.$render("i-checkbox", { id: "checkIndicators", caption: "Indicators?" }),
+                this.$render("i-checkbox", { id: "checkSwipe", caption: "Swipe?" })));
+        }
+    };
+    Config = __decorate([
+        components_2.customModule,
+        components_2.customElements("pageblock-carousel-config")
+    ], Config);
+    exports.default = Config;
+});
+define("@carousel/main", ["require", "exports", "@ijstech/components", "@carousel/main/data.json.ts", "@carousel/main/index.css.ts", "@carousel/main/config.tsx"], function (require, exports, components_3, data_json_1, index_css_1, config_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.Config = void 0;
+    exports.Config = config_1.default;
+    let Module1 = class Module1 extends components_3.Module {
+        constructor() {
+            super(...arguments);
+            this._data = {};
+            this.defaultEdit = true;
+        }
+        getData() {
+            return this._data;
+        }
+        async setData(data) {
+            this._data = data;
+            this.carouselConfig.data = data;
+            this.updateCarousel();
+        }
+        getTag() {
+            return this.tag;
+        }
+        async setTag(value) {
+            this.tag = value;
+        }
+        async edit() {
+            this.carouselConfig.data = this._data;
+            this.pnlCarousel.visible = false;
+            this.carouselConfig.visible = true;
+        }
+        async confirm() {
+            this._data = this.carouselConfig.data;
+            this.pnlCarousel.visible = true;
+            this.carouselConfig.visible = false;
+            this.updateCarousel();
+        }
+        async discard() {
+            this.pnlCarousel.visible = true;
+            this.carouselConfig.visible = false;
+        }
+        async config() { }
+        updateCarousel() {
+            this.isSwiping = false;
+            if (this._data.slidesToShow !== undefined)
+                this.carouselSlider.slidesToShow = this._data.slidesToShow;
+            this.carouselSlider.autoplay = this._data.autoplay;
+            this.carouselSlider.swipe = this._data.swipe;
+            this.btnPrev.visible = this._data.controls;
+            this.btnNext.visible = this._data.controls;
+            if (this._data.indicators)
+                this.carouselSlider.classList.add("--indicators");
+            else
+                this.carouselSlider.classList.remove("--indicators");
+            this.gridCarousel.templateColumns = this._data.controls ? ["45px", "auto", "45px"] : ["auto"];
+            this.carouselSlider.items = data_json_1.default.map(product => ({
+                name: undefined,
+                controls: [
+                    this.$render("i-image", { display: 'block', class: `--carousel-item ${product.url ? "pointer" : ""}`, width: "100%", padding: { left: '0.5em', right: '0.5em' }, url: product.img, onClick: () => {
+                            if (!this.isSwiping && product.url)
+                                window.open(product.url, "_self");
+                        } })
+                ]
+            }));
+        }
+        prev() {
+            if (!this._data.controls)
+                return;
+            this.carouselSlider.prev();
+        }
+        next() {
+            if (!this._data.controls)
+                return;
+            this.carouselSlider.next();
+        }
+        onSwipeStart() {
+            this.isSwiping = false;
+        }
+        onSwipeEnd(isSwiping) {
+            this.isSwiping = isSwiping;
+        }
+        render() {
+            return (this.$render("i-panel", { id: "pnlBlock", class: index_css_1.default },
+                this.$render("i-panel", { id: "pnlCarousel" },
+                    this.$render("i-grid-layout", { id: "gridCarousel", width: "100%", height: "100%", templateColumns: ["45px", "auto", "45px"] },
+                        this.$render("i-button", { id: "btnPrev", class: "--arrow-button", height: "100%", width: "45px", icon: { name: 'chevron-left', fill: 'rgba(160,168,177,.68)' }, background: { color: 'transparent' }, onClick: this.prev.bind(this) }),
+                        this.$render("i-carousel-slider", { id: "carouselSlider", width: "100%", height: "100%", onSwipeStart: this.onSwipeStart, onSwipeEnd: this.onSwipeEnd }),
+                        this.$render("i-button", { id: "btnNext", class: "--arrow-button", height: "100%", width: "45px", icon: { name: 'chevron-right', fill: 'rgba(160,168,177,.68)' }, background: { color: 'transparent' }, onClick: this.next.bind(this) }))),
+                this.$render("pageblock-carousel-config", { id: "carouselConfig", visible: false })));
+        }
+    };
+    Module1 = __decorate([
+        components_3.customModule
+    ], Module1);
+    exports.default = Module1;
+});
---
+++ modules/global/interface.ts
@@ -0,0 +1,32 @@
+interface IConfig {
+ slidesToShow?: number;
+ autoplay?: boolean;
+ controls?: boolean;
+ indicators?: boolean;
+ swipe?: boolean;
+}
+
+interface PageBlock {
+  // Properties
+  getData: () => any;
+  setData: (data: any) => Promise<void>;
+  getTag: () => any;
+  setTag: (tag: any) => Promise<void>
+  validate?: () => boolean;
+  defaultEdit?: boolean;
+  tag?: any;
+
+  // Page Events
+  readonly onEdit: () => Promise<void>;
+  readonly onConfirm: () => Promise<void>;
+  readonly onDiscard: () => Promise<void>;
+  // onClear: () => void;
+
+  // Page Block Events
+  edit: () => Promise<void>;
+  confirm: () => Promise<void>;
+  discard: () => Promise<void>;
+  config: () => Promise<void>;
+}
+
+export { IConfig, PageBlock };
